844bf87148fb67f2d19f5b23400e00354bdf0329
Alex Salazar <a2017113913@estudiantes.upsa.edu.bo>
2022-08-12 11:08:29 -0400

    update v3

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,12 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plost
+from PIL import Image
 
-
-st.write("MAKE YOUR FIRST SIMPLE APP ON STREAMLIT")
+st.set_page_config(layout="wide")
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+st.write("STREAMLIT BLOGS")
 st.write("""
-# Dashboard App 
+# WHAT IS A DASHBOARD? 
 
 """)
 
-
 st.write("The dashboard is like the gauges in the cockpit of an airplane (although it should be a bit simpler!).\n it's a super-powerful tool for getting insights from data and centralizing the KPIs you need to know what's really going on with your business. \n In this article we are going to see what a dashboard is, why it is a good idea to implement it in your company and the main metrics to include according to your objectives")
+
+st.write("""
+# WHAT IS A DASHBOARD? 
+
+""")
 
